a1496eb17f6431902efe00499d6cc4fa59b96c45
Daniyyel Yosef Bey <coding4you.net@gmail.com>
2020-05-12 20:51:23 -0400

    starting laravel/react project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <title>Wholistic Vitality</title>
+    <meta property="og:title" content="Wholistic Vitality">
+    <meta name="description" content="Health and Wellness website">
+    <meta property="og:description" content="Health and Wellness website">
+    <meta property="og:image" content="">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:site_name" content="Coding4You">
+    <meta name="twitter:image:alt" content="Learn how to live your best life and empower your self with real solutions to health.">
+    <meta name="twitter:site" content="@coding4you">
+    <meta property="og:url" content="https://coding4you.net">
+    <link rel="canonical" href="/">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"><meta name="theme-color" content="#ad38f1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/iconsfavicon.png">
+    <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="stylesheet" href="/css/app.css">
+    {{-- <link rel="stylesheet" href="/css/styles.css"> --}}
+    <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  </head>
+  <body>
+    <section id="welcome">
+      <div class="welcome-menu">
+        <div class="logo">
+          <img src="/img/logo.png" alt="logo">
+        </div>
+        <div class="menu">
+          <div class="menu-title">
+            Menu
+          </div>
+          <ul class="links">
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Wait List</a>
+            </li>
+            <li>
+              <a href="#">Gift Cards</a>
+            </li>
+            <li>
+              <a href="#">Offers</a>
+            </li>
+          </ul>
+        </div>
+        <div class="social-icons">
+          <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+          <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div class="location">
+          <div class="address">
+            1300 Bey Street, <br>
+            Miami, Florida 11367
+          </div>
+          <div class="phone-number">
+            <a href="tel:7182198652">718-219-8652</a>
+          </div>
+        </div>
+      </div>
+      <div class="welcome-jumbo">
+        <div class="status">NEW</div>
+        <h1>Wholistic Vitality</h1>
+        <img class="wholistic" src="img/Wheel of Wellbeing.png" class="wellbeing" alt="logo">
+      </div>
+    </section>
+    
+  </body>
+</html>
